dfdb7eb8555d272f1759e03b40f88b2918921595
a2k11 <adam.kaewell@gmail.com>
2014-11-18 15:53:13 -0500

    Hound changes

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,22 +1,22 @@
 MoneyRails.configure do |config|
 
-config.default_currency = :usd
+  config.default_currency = :usd
 
-config.amount_column = { prefix: '',           # column name prefix
-                         postfix: '_cents',    # column name postfix
-                         column_name: nil,
-                         type: :integer,       # column type
-                         present: true,        # column will be created
-                         null: false,
-                         default: 0
-}
-
-config.currency_column = { prefix: '',
-                           postfix: '_currency',
+  config.amount_column = { prefix: "",           # column name prefix
+                           postfix: "_cents",    # column name postfix
                            column_name: nil,
-                           type: :string,
-                           present: true,
+                           type: :integer,       # column type
+                           present: true,        # column will be created
                            null: false,
-                           default: 'USD'
-}
+                           default: 0
+  }
+
+  config.currency_column = { prefix: "",
+                             postfix: "_currency",
+                             column_name: nil,
+                             type: :string,
+                             present: true,
+                             null: false,
+                             default: "USD"
+  }
 end
