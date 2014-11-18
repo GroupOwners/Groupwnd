2951f74cecd73e5d74e5243b01cc6daa8bff954b
a2k11 <adam.kaewell@gmail.com>
2014-11-18 08:47:02 -0500

    Added Offer Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117200904) do
+ActiveRecord::Schema.define(version: 20141118134311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "sale_price"
+    t.decimal  "actual_price"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
